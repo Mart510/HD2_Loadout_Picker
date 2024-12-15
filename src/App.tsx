2fad12dc5ea86e31509206b0e9ad704a98b06f52
Mart510 <martynandrewbillings@gmail.com>
2024-12-15 11:30:29 +0000

    reconfigure for vercel deploy

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div className='flex'>
+      <div className='flex flex-row'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
